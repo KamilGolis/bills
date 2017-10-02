75c84b0b54dcadc5a632d692951673f8f4e95d8b
Kamil Golis <trot.dms@gmail.com>
2017-10-02 09:11:26 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO category (category_id, name, icon) VALUES (30, 'Główna', 'glyphicon-star');
-INSERT INTO category (category_id, name, icon) VALUES (31, 'Kosz', 'glyphicon-trash');
+--INSERT INTO category (category_id, name, icon) VALUES (30, 'Główna', 'glyphicon-star');
+--INSERT INTO category (category_id, name, icon) VALUES (31, 'Kosz', 'glyphicon-trash');
 
-INSERT INTO status (status_id, name, status_colour) VALUES (10, 'Zapłacone', 'success');
-INSERT INTO status (status_id, name, status_colour) VALUES (11, 'Do zapłaty', 'danger');
-INSERT INTO status (status_id, name, status_colour) VALUES (12, 'Do wyjaśnienia', 'warning');
-INSERT INTO status (status_id, name, status_colour) VALUES (13, 'Inne', 'warning');
+--INSERT INTO status (status_id, name, status_colour) VALUES (10, 'Zapłacone', 'success');
+--INSERT INTO status (status_id, name, status_colour) VALUES (11, 'Do zapłaty', 'danger');
+--INSERT INTO status (status_id, name, status_colour) VALUES (12, 'Do wyjaśnienia', 'warning');
+--INSERT INTO status (status_id, name, status_colour) VALUES (13, 'Inne', 'warning');
 
-INSERT INTO loan_holders (loan_holder_id, address, bank_account_number, description, name) VALUES (20, 'ul. Felińskiego 8, 93-259 Łódź', '12111122223333444455556666', 'Administracja Aktus', 'Aktus');
-INSERT INTO loan_holders (loan_holder_id, address, bank_account_number, description, name) VALUES (21, '-', '12666655554444333322221111', '-', 'Puste');
+--INSERT INTO loan_holders (loan_holder_id, address, bank_account_number, description, name) VALUES (20, 'ul. Felińskiego 8, 93-259 Łódź', '12111122223333444455556666', 'Administracja Aktus', 'Aktus');
+--INSERT INTO loan_holders (loan_holder_id, address, bank_account_number, description, name) VALUES (21, '-', '12666655554444333322221111', '-', 'Puste');
 
-INSERT INTO users (user_id, email, password_hash, role) VALUES (1, 'demo@gmail.com', '$2a$10$ebyC4Z5WtCXXc.HGDc1Yoe6CLFzcntFmfse6/pTj7CeDY5I05w16C', 'ADMIN');
-INSERT INTO users (user_id, email, password_hash, role) VALUES (2, 'admin@admin.com', '$2a$10$hZ6zN/33EqBmV7APXgABYOVqEvCKZwMHH4qovDDHjtDuQ59HR8QtS', 'ADMIN');
-INSERT INTO users (user_id, email, password_hash, role) VALUES (3, 'test@test.com', '$2a$10$wQvQgpvWgCe.LE4yATZrZOy1lEhD3kBZ2Ie9DS5piqs7twHHWJakS', 'USER');
-INSERT INTO users (user_id, email, password_hash, role) VALUES (4, 'sda@sda.pl', '$2a$10$phykRx2lkDRzqkdeh.AXsuSV.tNQOBJTzwLJjxECjjjgFjY50A0qa', 'USER');
-INSERT INTO users (user_id, email, password_hash, role) VALUES (5, 'asdf@gmail.com', '$2a$10$Mq3BbxsV.cyroUxg7bKgbO.Gh9mJR1QhhfofVhzS.bYseBuJ/WLIS', 'USER');
+--INSERT INTO users (user_id, email, password_hash, role) VALUES (1, 'demo@gmail.com', '$2a$10$ebyC4Z5WtCXXc.HGDc1Yoe6CLFzcntFmfse6/pTj7CeDY5I05w16C', 'ADMIN');
+--INSERT INTO users (user_id, email, password_hash, role) VALUES (2, 'admin@admin.com', '$2a$10$hZ6zN/33EqBmV7APXgABYOVqEvCKZwMHH4qovDDHjtDuQ59HR8QtS', 'ADMIN');
+--INSERT INTO users (user_id, email, password_hash, role) VALUES (3, 'test@test.com', '$2a$10$wQvQgpvWgCe.LE4yATZrZOy1lEhD3kBZ2Ie9DS5piqs7twHHWJakS', 'USER');
+--INSERT INTO users (user_id, email, password_hash, role) VALUES (4, 'sda@sda.pl', '$2a$10$phykRx2lkDRzqkdeh.AXsuSV.tNQOBJTzwLJjxECjjjgFjY50A0qa', 'USER');
+--INSERT INTO users (user_id, email, password_hash, role) VALUES (5, 'asdf@gmail.com', '$2a$10$Mq3BbxsV.cyroUxg7bKgbO.Gh9mJR1QhhfofVhzS.bYseBuJ/WLIS', 'USER');
